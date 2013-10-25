19c9b873f75de0c32a5e22b36495cd51c43d96ac
nvcHilary <hilary@nexvend.com>
2013-10-25 22:31:54 +0800

    allow cross platform installation

---
+++ js/mozappinstall.js
@@ -0,0 +1,12 @@
+var manifestLocation = "/manifest.webapp"; // your domain here
+var installRequest = navigator.mozApps.install(manifestLocation);
+
+installRequest.onsuccess = function(data) {
+    // App installed successfully!
+    console.log("Install successful!");
+};
+
+installRequest.onerror = function(err) {
+    // App couldn't be installed!
+    console.log("Install error!");
+};
